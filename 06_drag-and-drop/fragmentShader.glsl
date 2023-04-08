c114e97468a51ab4104df3aaf682999b37218244
tomes <kutlakt@yahoo.com>
2023-04-08 15:50:51 +0200

    donezo

--- 06_drag-and-drop/fragmentShader.glsl
+++ 06_drag-and-drop/fragmentShader.glsl
@@ -1,8 +1,15 @@
 #version 430
 
 out vec4 color;
+uniform int vaoIndex;
+
 
 void main(void)
 {
-	color = vec4(0.0, 1.0, 0.0, 1.0);
+	if(vaoIndex == 0)
+		color = vec4(1,0,0,0);
+	if(vaoIndex == 1)
+		color = vec4(0,1,0,0);
+	if(vaoIndex == 2)
+		color = vec4(0,0,1,0);
 }
